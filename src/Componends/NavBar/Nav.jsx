473fa1218a2ec63d7093c3e47d51ff0fc181cae7
Adil <ajkkalappatt@gmail.com>
2022-01-03 19:00:16 +0530

    added logo

--- src/Componends/NavBar/Nav.jsx
+++ src/Componends/NavBar/Nav.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Logo1 from './img/Logo.png';
+import Logo2 from './img/Logo2.png';
 import './Nav.css'
+
 
 function Nav() {
     return (
         <div>
             <nav className="nav">
-                <img className="imgLogo" src="./img/logo.png" alt="" />
+                <img className="imgLogo" src={Logo1} alt="" />
                
-                <img className="imgLogo2" src="./img/logo2.png" alt="" />
+                <img className="imgLogo2" src={Logo2} alt="" />
                  <h2 align="center" className="Heading-main">Quantitative techniques</h2>
             </nav>
         </div>
