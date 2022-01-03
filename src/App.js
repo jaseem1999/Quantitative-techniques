d9fc6f6d3dbbba540acff5cfcbd0e096f8a84cdc
Adil <ajkkalappatt@gmail.com>
2022-01-03 17:37:54 +0530

    first committ

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Nav from './Componends/NavBar/Nav.jsx';
+
+function App() {
+  ///Correlation
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componends/NavBar/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div>
+            <nav className="nav">
+                <img className="imgLogo" src="./img/logo.png" alt="" />
+               
+                <img className="imgLogo2" src="./img/logo2.png" alt="" />
+                 <h2 align="center" className="Heading-main">Quantitative techniques</h2>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
