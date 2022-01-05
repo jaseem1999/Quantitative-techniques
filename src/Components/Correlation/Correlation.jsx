283995c843b6f43a5d751716e8d8715158f6437c
Adil <ajkkalappatt@gmail.com>
2022-01-05 22:28:20 +0530

    edited

--- src/Components/Correlation/Correlation.jsx
+++ src/Components/Correlation/Correlation.jsx
@@ -4,7 +4,7 @@ import './Correlation.css'
 
 
 function Correlation() {
-    let variables =[{x : 24 , y : 18},{x : 27 , y : 25},{x : 26 , y : 26},{x : 30 , y : 29}] 
+    let variables =[{x : 24 , y : 22},{x : 26 , y : 22},{x : 28 , y : 23},{x : 32 , y : 26},{x : 38 , y : 34},{x : 40 , y : 36},{x : 40 , y : 35},{x : 42 , y : 38},{x : 44 , y : 40},{x : 46 , y : 40}] 
 
     /// calculation of arithemetic mean r = Σ(𝑥−𝑥)(𝑦−𝑦)√Σ(𝑥−𝑥)2 Σ(𝑦−𝑦)2
     let x_mean = variables.reduce((acc, curr) => acc + curr.x, 0) / variables.length
@@ -18,7 +18,7 @@ function Correlation() {
     // if (r === 0) return no correlation
     let result = r < 0.75 ? "morderate degree of positive correlation" : r > 0.75 ? "high degree of positive correlation" : 
                  r <0.50 ? "Low degree of positive correlation" : r === -1 ? "prefect negative correlation" : 1 === r ? "prefect positive correlation" : r === 0 ? "no correlation":
-                 r > -0.75 ? "high degree of neghative correlation" : r < 0.75 ? "morderate degree of Neghative correlation" : "Low degree of neghative correlation"
+                 r > -0.75 ? "high degree of neghative correlation" : r < -0.75 ? "morderate degree of Neghative correlation" : "Low degree of neghative correlation"
 
 
 
@@ -55,9 +55,9 @@ function Correlation() {
                 </div>
                 <div className="correlation-t">
                     <p>
-                        A husbend and wife correlation is a measure of the strength of the relationship between two variables.<br/>
-                        X variables is defined husbend<br/>
-                        Y variables is defined wife<br/>
+                       From the following data related to the ages of 10 couples, state whether there is any correlation between ages of husband and wife.<br/>
+                        X variables is defined as husband<br/>
+                        Y variables is defined as wife<br/>
                         <b>given the following data:</b><br/>
                     </p>
                     <table class="table">
